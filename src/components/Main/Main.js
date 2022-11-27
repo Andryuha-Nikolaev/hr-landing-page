c2515c69a6b692866288ec384745689197e7c032
Tatyana Izmailova <rutaizm@yandex.ru>
2022-11-27 17:09:17 +0300

    section 2 draft

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Share from '../Share/Share';
 
 function Main() {
   return (
     <main>
-      <h1>тут будет блок</h1>
+      <Share/>
       <h1>тут будет блок2</h1>
       <h1>тут будет блок3</h1>
       <h1>тут будет блок4</h1>
