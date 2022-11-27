0668958aa5a5ab72a180f5e685286be952397824
Gleb Lyashenkov <glepka17@yandex.ru>
2022-11-27 19:26:36 +0300

    feat: верстка блока Tasks

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Share from '../Share/Share';
+import React from "react";
+import Share from "../Share/Share";
+import Tasks from "../Tasks/Tasks";
 
 function Main() {
   return (
     <main>
-      <Share/>
+      <Share />
+      <Tasks />
       <h1>тут будет блок2</h1>
       <h1>тут будет блок3</h1>
       <h1>тут будет блок4</h1>
