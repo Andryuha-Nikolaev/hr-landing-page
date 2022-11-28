2dec692d8fc50be994c02f9baafeff05eb0f5803
admin <andryuha_nikolaev@mail.ru>
2022-11-28 15:55:48 +0300

    feat#1: доделал секцию факультеты

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Share from "../Share/Share";
 import Faculty from "../Faculty/Faculty";
-import VacanciesList from '../VacanciesList/VacanciesList';
-import vacancies from '../../assets/vacancies.json'
-import Share from "../Share/Share";
+import VacanciesList from "../VacanciesList/VacanciesList";
+import vacancies from "../../assets/vacancies.json";
+import facultyList from "../../assets/facultyList.json";
+import facultyInfo from "../../assets/facultyInfo.json";
 import Tasks from "../Tasks/Tasks";
 
 function Main() {
@@ -15,7 +16,7 @@ function Main() {
       <h1>тут будет блок3</h1>
       <h1>тут будет блок4</h1>
       <h1>тут будет блок5</h1>
-      <Faculty />
+      <Faculty facultyList={facultyList} facultyInfo={facultyInfo} />
       <VacanciesList vacancies={vacancies} />
     </main>
   );
