b699c91ca4f2e1c0268539e24f58d6a79ad0586d
Tatyana Izmailova <rutaizm@yandex.ru>
2022-11-29 13:22:42 +0300

    vacancielist refactoring

--- src/components/VacanciesList/VacanciesList.js
+++ src/components/VacanciesList/VacanciesList.js
@@ -1,30 +1,35 @@
 import React from "react";
 import "./VacanciesList.css";
 import Vacancy from "../Vacancy/Vacancy";
-import clsx from 'clsx';
+import clsx from "clsx";
 
 function VacanciesList({ vacancies, reviews }) {
+  const [toggleState, setToggleState] = React.useState("vacancies");
+  const [openVacancie, setOpenVacancie] = React.useState(false);
+  const [openReview, setOpenReview] = React.useState(false);
 
-  const [toggleState, setToggleState] = React.useState(1);
-  const [openList, setOpenList] =  React.useState(false);
-
-  function toggleTab(index) {
-    setToggleState(index);
+  function toggleTab() {
+    toggleState === "vacancies" ? setToggleState("reviews") : setToggleState("vacancies");
   }
 
-function toggleList() {
-  openList === false ? setOpenList(true) : setOpenList(false)
-}
-
+  function toggleList() {
+    if (toggleState === "vacancies") {
+      openVacancie === false ? setOpenVacancie(true) : setOpenVacancie(false);
+    } else {
+      openReview === false ? setOpenReview(true) : setOpenReview(false);
+    }
+  }
 
   return (
     <section className="vacancies">
       <h2 className="vacancies__title">Вакансии</h2>
       <button
-        className={toggleState === 1
+        className={
+          toggleState === "vacancies"
             ? "vacancies__btn vacancies__btn_active"
-            : "vacancies__btn "}
-        onClick={() => toggleTab(1)}
+            : "vacancies__btn "
+        }
+        onClick={() => toggleTab()}
       >
         Для наставников
       </button>
@@ -34,33 +39,46 @@ function toggleList() {
             ? "vacancies__btn vacancies__btn_active"
             : "vacancies__btn "
         }
-        onClick={() => toggleTab(2)}
+        onClick={() => toggleTab()}
       >
         Для ревьюеров
       </button>
 
       <ul
-        className={clsx (toggleState === 1
+        className={clsx(
+          toggleState === "vacancies"
             ? "vacancies__list vacancies__list_active"
-            : "vacancies__list", openList ? "vacancies__list vacancies__list_shown " : "vacancies__list" )
-          }
-
-        >
-        {vacancies.map((vacancy) => (
-          <Vacancy vacancy={vacancy} key={vacancy.name} />
+            : "vacancies__list",
+          openVacancie
+            ? "vacancies__list vacancies__list_shown "
+            : "vacancies__list"
+        )}
+      >
+        {vacancies.map((vacancy, index) => (
+          <Vacancy vacancy={vacancy} key={index} />
         ))}
       </ul>
 
-      <ul className={clsx (toggleState === 2
+      <ul
+        className={clsx(
+          toggleState === "reviews"
             ? "vacancies__list vacancies__list_active"
-            : "vacancies__list", openList ? "vacancies__list vacancies__list_shown " : "vacancies__list" )
-        }>
-        {reviews.map((review) => (
-          <Vacancy vacancy={review} key={review.name} />
+            : "vacancies__list",
+          openReview
+            ? "vacancies__list vacancies__list_shown "
+            : "vacancies__list"
+        )}
+      >
+        {reviews.map((review, index) => (
+          <Vacancy vacancy={review} key={index} />
         ))}
       </ul>
 
-      <button type="button" className="vacancies__see-all" onClick={() => toggleList()}>
+      <button
+        type="button"
+        className="vacancies__see-all"
+        onClick={() => toggleList()}
+      >
         Посмотреть все
       </button>
     </section>
