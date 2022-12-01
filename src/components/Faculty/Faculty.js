ac4308db0143586f97b493e1c430f4ef7dab236c
admin <andryuha_nikolaev@mail.ru>
2022-12-02 00:52:03 +0300

    feat#87: мобилка секции факультеты

--- src/components/Faculty/Faculty.js
+++ src/components/Faculty/Faculty.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "./Faculty.css";
-// import arrowBlack from "../../images/arrow-black.svg";
+import arrowBlue from "../../images/faculty-arrow.svg";
+import arrowBlueUp from "../../images/arrow-blue-up.svg";
 import faculty1 from "../../images/faculty1.png";
 
 function Faculty({ facultyList, facultyInfo }) {
+  const [open, setOpen] = useState(false);
+
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const sortName = facultyList[activeIndex];
 
   const onClickCategory = (index) => {
     setActiveIndex(index);
+    setOpen(false);
   };
 
   const facultyInfoActive = facultyInfo[activeIndex];
@@ -15,7 +21,7 @@ function Faculty({ facultyList, facultyInfo }) {
   return (
     <section className="faculty">
       <div className="faculty__container">
-        <h2 className="faculty__title">Факультеты</h2>
+        <h2 className="faculty__title">Наши факультеты</h2>
         <div className="faculty__content">
           <ul className="faculty__list">
             {facultyList.map((value, i) => (
@@ -32,6 +38,36 @@ function Faculty({ facultyList, facultyInfo }) {
               </li>
             ))}
           </ul>
+          <div className="faculty__mobile-menu">
+            <p className="faculty__popup-change" onClick={() => setOpen(!open)}>
+              {sortName}
+              <img
+                src={!open ? arrowBlue : arrowBlueUp}
+                alt="развернуть меню"
+                className="faculty__popup-arrow"
+              />
+            </p>
+            {open && (
+              <div className="faculty__popup">
+                <ul className="faculty__popup-list">
+                  {facultyList.map((name, i) => (
+                    <li
+                      key={i}
+                      onClick={() => onClickCategory(i)}
+                      className={
+                        activeIndex === i
+                          ? "faculty__popup-item faculty__popup-item_active"
+                          : "faculty__popup-item"
+                      }
+                    >
+                      {name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+
           <div className="faculty__info-container">
             <div className="faculty__info">
               <p className="faculty__text">{facultyInfoActive.description}</p>
