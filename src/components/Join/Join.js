c8c5dbc1e5ceb8c9417c139a4dd6f9a9635b871a
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2023-02-09 20:58:47 +0300

    refactor: join, questions, footer

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -11,36 +11,41 @@ function Join({
 }) {
   return (
     <section className="join">
-      <h2 className="join__header">Не нашли подходящей роли?</h2>
-      <p className="join__text">
-        Если вам интересно поработать с нами, заполните заявку. Мы постараемся
-        подобрать вакансию под ваш опыт
-      </p>
-      <div
-        className={`pop-up ${formOpen && "pop-up_opened"}`}
-        onClick={handleOverlayClick}
-      >
-        <div className="pop-up__container">
-          <Form onClose={handleFormClose} />
+      <div className="join__contant">
+        <h2 className="join__header">Не нашли подходящей роли?</h2>
+        <p className="join__text">
+          Если вам интересно поработать с нами, заполните заявку. Мы постараемся
+          подобрать вакансию под ваш опыт
+        </p>
+        <div
+          className={`pop-up ${formOpen && "pop-up_opened"}`}
+          onClick={handleOverlayClick}
+        >
+          <div className="pop-up__container">
+            <Form onClose={handleFormClose} />
+          </div>
         </div>
+        <button className="join-btn" type="button" onClick={handleOpenForm}>
+          Присоединиться к команде
+        </button>
+        <img
+          className="join__img_star"
+          src={images.star}
+          alt="падающая звезда"
+        />
+        <img
+          className="join__img__blue-arrow"
+          src={images.blueArrow}
+          alt="синяя стрелка"
+        />
+        <img
+          className="join__img_programmer"
+          src={images.programmer}
+          alt="программист"
+        />
       </div>
-      <button className="join-btn" type="button" onClick={handleOpenForm}>
-        Присоединиться к команде
-      </button>
-      <img className="join__img_star" src={images.star} alt="падающая звезда" />
-      <img
-        className="join__img__blue-arrow"
-        src={images.blueArrow}
-        alt="синяя стрелка"
-      />
-      <img
-        className="join__img_programmer"
-        src={images.programmer}
-        alt="программист"
-      />
     </section>
   );
-
 }
 
 export default Join;
